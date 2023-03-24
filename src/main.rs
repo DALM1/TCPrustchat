d966648fe4fc8829bf7e1a43757a817c2d9b62ca
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 02:35:08 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod server;
 mod client;
 
 fn main() {
-    // Démarrer le serveur dans un thread séparé
+   
     std::thread::spawn(|| {
         server::main().unwrap();
     });
 
-    // Démarrer le client dans le thread principal
+    
     client::main().unwrap();
 }
